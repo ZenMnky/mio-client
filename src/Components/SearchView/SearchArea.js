e2350abf893ad009821acb45d04e124f6027afc8
Justin Hager <coachjustinhager@gmail.com>
2020-12-27 14:12:04 -0800

    stateful search component setup

--- src/Components/SearchView/SearchArea.js
+++ src/Components/SearchView/SearchArea.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
+import { AppContext } from '../../Context/AppContext';
 
 
 export default class SearchArea extends Component {
+    static contextType = AppContext;
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            searchTerms: '',
+            relationshipZone: 0
+        }
+    }
+
+    relationshipZoneChanged = (relationshipZone) => {
+        this.setState({
+            relationshipZone
+        })
+    }
+
+    handleSearchTermsChanged = (searchTerms) => {
+        this.setState({
+            searchTerms
+        })
+    }
+
     render(){
         return(
             
             <section id="searchRelationship" className='searchRelationship'>
                 <form action="">
                     <div>
-                        <label for="search_terms">Search Terms</label>
-                        <input id="search_terms" type="text" placeholder="by name or nickname" />
+                        <label htmlFor="search_terms">Search Terms</label>
+                        <input 
+                            id="search_terms" 
+                            type="text" 
+                            placeholder="by name or nickname"
+                            value={this.state.searchTerms}
+                            onChange={e => this.handleSearchTermsChanged(e.target.value)}
+                        />
                     </div>   
                 
                     
                     <div>
-                        <label for="selectRelationshipZone">
+                        <label htmlFor="selectRelationshipZone">
                             Filter by Zone:
                         </label>
                         <select 
                             name="relationshipZone" 
                             id="selectRelationshipZone"
+                            value={this.state.relationshipZone}
+                            onChange={e => this.relationshipZoneChanged(e.target.value)}
                             >
+                            <option value="0">Any</option>
                             <option value="1">Zone 1</option>
                             <option value="2">Zone 2</option>
                             <option value="3">Zone 3</option>
